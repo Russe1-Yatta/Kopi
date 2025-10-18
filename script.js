6c9041227621a117af6dd459533297b3b4931f74
James Sicat <sicatjamesrussel@gmail.com>
2025-10-19 00:16:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -32,10 +32,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
     if (heroImage && heroText) {
         const images = [
-            '../img/home/mhome1.png',
-            '../img/home/mhome2.png',
-            '../img/home/mhome3.png',
-            '../img/home/mhome4.png'
+            'img/home/mhome1.png',
+            'img/home/mhome2.png',
+            'img/home/mhome3.png',
+            'img/home/mhome4.png'
         ];
         const texts = ['', '', '', ''];
         let currentIndex = 0;
@@ -57,5 +57,6 @@ document.addEventListener('DOMContentLoaded', () => {
         setInterval(changeHeroContent, 4000);
     }
 });
+
 
 
